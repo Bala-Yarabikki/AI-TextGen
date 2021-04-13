db78fc19eca4974dc2d0412ed4facd917225a632
Bala-Yarabikki <60878131+Bala-Yarabikki@users.noreply.github.com>
2021-04-13 22:13:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ st.title("AI TextGen App(Bala)")
 ai = aitextgen()
 
 #prompt_text = "Machine Learning is awesome"
-prompt_text = st.text_input(label="Enter input text",
-              value="Machine learning is Beautiful")
+prompt_text = st.text_input(label="Enter input text", value="Machine learning is Beautiful")
 
-with st.spinner("AI is on force........."):
+@st.cache()
+def spinner():
+    with st.spinner("AI is on force........."):
+        gpt_text = ai.generate_one(prompt=prompt_text, max_length=200)
+    return gpt_text
 
-    gpt_text = ai.generate_one(prompt=prompt_text,max_length=100)
-#st.balloons()
+# #st.balloons()
 st.success("Machine Learning Generated the text")
-print(gpt_text)
-st.text(gpt_text)
+st.text(spinner())
